84bbf6b9da494229bf92214f69f76c67651c2681
jingkang97 <jingkang@lomotif.com>
2021-09-28 14:39:55 +0800

    feat: indiv stats

--- client/src/components/Session/Analytics.js
+++ client/src/components/Session/Analytics.js
@@ -11,6 +11,7 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
     const [evaluation, setEvaluation] = useState(null)
     const [danceScore, setDanceScore] = useState(0)
     const [moveChart, setMoveChart] = useState([])
+    const [syncDelayDisplay, setSyncDelayDisplay] = useState(0)
 
     const [loading, setLoading] = useState(false)
     const getEvaluation = async() => {
@@ -24,76 +25,79 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
           }
     }
     const calculateSyncDelay = () => {
+        let count = 0;
         for(let i = 0; i < syncDelay.length; i++){
             console.log(syncDelay[i])
+            count += syncDelay[i]
+        }
+        if(syncDelay.length != 0){
+            setSyncDelayDisplay((count/syncDelay.length).toFixed(1))
         }
     }
 
+
+    
     const calculateIndividualDance = () => {
         let userDance = []
         let moveChartList = []
 
         for(let i = 0; i < session.length; i += 1){
             userDance.push({username: session[i].username, userId: session[i].userId, movePercent: '0%', positionPercent:'0%'})
-            moveChartList.push({username: session[i].username, userId: session[i].userId, dataMove: [{name: 'correct', value: 0}, {name: 'wrong', value: 0}]})
+            moveChartList.push({username: session[i].username, userId: session[i].userId, dataMove: [{name: 'correct', value: 0}, {name: 'wrong', value: 0}], dataPosition: [{name: 'correct', value: 0}, {name: 'wrong', value: 0}]})
         }
         
 
         let moveScore = 0
         let moveTotal = 0
-        let wrongScore = 0
+        let wrongMoveScore = 0
+
         for(let j = 0; j < session.length; j += 1){
             moveScore = 0
             moveTotal = 0
-            if(evaluation != null){
-                for(let i = 0; i < evaluation.datas.length; i++){
-                    if(i < session[j].session.length){
-                        moveTotal += 1
-                        if(session[j].session[i].danceMove == evaluation.datas[i].danceMove){
-                            moveScore += 1
-                            userDance[j].movePercent = `${((moveScore/moveTotal)*100).toFixed(1)}%`
-                            moveChartList[j].dataMove[0].value = parseFloat(((moveScore/moveTotal)*100).toFixed(1))
-
-                        }
-                        else{
-                            wrongScore += 1
-                            moveChartList[j].dataMove[1].value = parseFloat(((wrongScore/moveTotal)*100).toFixed(1))
-                            // console.log('no')
-                        }
-                    }
+            wrongMoveScore = 0
+            for(let i = 0; i < session[j].session.length; i++){
+                moveTotal += 1
+                if(session[j].session[i].danceMove == evaluation.datas[i].danceMove){
+                    moveScore += 1
+                          
                 }
-            }
-        }
+                else{
+                    wrongMoveScore += 1
+                }
 
-        setMoveChart(moveChartList)
+                console.log('wrongscore', wrongMoveScore)
+                console.log('move', moveScore)
+                console.log('outside movetotal',moveTotal)
+            }           
+            userDance[j].movePercent = ((moveScore/moveTotal)*100).toFixed(1)              
+            moveChartList[j].dataMove[0].value = parseFloat(((moveScore/moveTotal)*100).toFixed(1))
+            moveChartList[j].dataMove[1].value = parseFloat(((wrongMoveScore/moveTotal)*100).toFixed(1))
+
+        }
         
         let danceScore = 0
         let danceTotal = 0
+        let wrongDanceScore = 0
         for(let j = 0; j < session.length; j += 1){
             danceScore = 0
             danceTotal = 0
-            if(evaluation != null){
-                for(let i = 0; i < evaluation.datas.length; i++){
-                    if(i < session[j].session.length){
-                        danceTotal += 1
-                        // CHANGE CSV DATA FIRST
-                        let temp = evaluation.datas[i].position.split(',')
-                        console.log(temp)
-                        // console.log(session[j].session[i].position)
-                        console.log(temp.indexOf(session[j].session[i].position))
-                        if(temp.indexOf(session[j].session[i].position) + 1 == session[j].session[i].position){
-                            danceScore += 1
-                            userDance[j].positionPercent = `${((danceScore/danceTotal)*100).toFixed(1)}%`
-                        }
-                        else{
-                            // console.log('no')
-                        }
+            wrongDanceScore = 0
+            for(let i = 0; i < session[j].session.length; i++){
+                    danceTotal += 1
+                    let temp = evaluation.datas[i].position.split(',')
+                    if(temp.indexOf(session[j].session[i].position) + 1 == session[j].session[i].position){
+                        danceScore += 1
+                    }
+                    else{
+                        wrongDanceScore += 1
+                        moveChartList[j].dataPosition[1].value = parseFloat((wrongDanceScore).toFixed(1))
                     }
                 }
-            }
+            moveChartList[j].dataPosition[1].value = parseFloat(((wrongDanceScore/danceTotal)*100).toFixed(1))
+            userDance[j].positionPercent = ((danceScore/danceTotal)*100).toFixed(1)
+            moveChartList[j].dataPosition[0].value = parseFloat(((danceScore/danceTotal)*100).toFixed(1))
         }
-
-
+        setMoveChart(moveChartList)
         setDanceScore(userDance)
     }
 
@@ -139,8 +143,11 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
                                     </div>
                                     <div style={{fontSize:'20px', color:'white', position:'relative', width:'100%', height:'100%', background:'transparent', alignItems:'center', display:'flex', flexDirection:'column'}}>
                                     <div style={{position:'absolute', top:'50%', left:'50%', transform:'translate(-50%, -50%', display:'flex', flexDirection:'column', alignItems:'center', fontWeight:'bold', justifyContent:'center', width:'100%'}}>
-                                        <div>{item.movePercent}</div>
+                                        <div>{item.movePercent}%</div>
                                         <div>Correct</div>
+                                        {/* TODO */}
+                                        {item.movePercent == 100.0 ? <div>Perfect</div> : (item.movePercent < 100.0 )} 
+
                                     </div>
                                     <DoughnutChart data={moveChart[moveChart.findIndex(x=> x.userId == item.userId)].dataMove}/>
                                     
@@ -156,11 +163,11 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
                                     </div>
                                     <div style={{fontSize:'20px', color:'white', position:'relative', width:'100%', height:'100%', background:'transparent', alignItems:'center', display:'flex', flexDirection:'column'}}>
                                     <div style={{position:'absolute', top:'50%', left:'50%', transform:'translate(-50%, -50%', display:'flex', flexDirection:'column', alignItems:'center', fontWeight:'bold', justifyContent:'center', width:'100%'}}>
-                                        <div>{item.positionPercent}</div>
+                                        <div>{item.positionPercent}%</div>
                                         <div>Correct</div>
                                     </div>
-                                    <DoughnutChart data={moveChart[moveChart.findIndex(x=> x.userId == item.userId)].dataMove}/>
-                                    
+                                    <DoughnutChart data={moveChart[moveChart.findIndex(x=> x.userId == item.userId)].dataPosition}/>
+    
                                     </div>
                                    
                                 </div>
@@ -202,7 +209,7 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
 
                 {/* emg */}
                 <Col md={24}>
-                    <div style={{width:'100%', height:'100px', backgroundColor:'#3A3C41', borderRadius:'10px', display:'flex', flexDirection:'row', alignItems:'center', boxShadow: '0px 0px 20px 1px #202225', justifyContent:'space-evenly'}}>
+                    <div style={{width:'100%', height:'100%', backgroundColor:'#3A3C41', borderRadius:'10px', display:'flex', flexDirection:'row', alignItems:'center', boxShadow: '0px 0px 20px 1px #202225', justifyContent:'space-evenly'}}>
                     <div style={{height:'100%', width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
                         Fatigue Level
                         <div>...</div>
@@ -210,7 +217,31 @@ const Analytics = ({stop, rows, session, emg, syncDelay, start, end}) => {
                     </div>
                     <div style={{height:'100%', width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
                         Average Sync Delay
-                        <div>...</div>
+                        <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-around', width:'inherit', background:'transparent', width:'100%'}}>
+                          
+                          {/* {syncDelay.length ?  (syncDelay[syncDelay.length-1] == 0 ? 
+                          <div className="perfectSync">Perfect Sync</div> : (
+                            syncDelay[syncDelay.length-1] > 0 && syncDelay[syncDelay.length-1] <= 0.5 ? 
+                            <div className="okSync">Almost Perfect Sync</div> : (syncDelay[syncDelay.length-1] > 0.5 ? <div className="notSync">Please Match Up!</div> : null)
+                            
+                          )) : <div style={{fontSize:'30px'}}>Get Ready...</div>}
+                           */}
+                          
+                            {/* Almost Perfect Sync! */}
+                            {/* Please Match Up! */}
+                            {/*  */}
+                          <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                            Off By
+                            <div style={{display:'flex', flexDirection:'column',height:'100%', background:'transparent', position:'relative', color:'white'}}>
+                              <IoIosSync className="syncMove"/>
+                              <div style={{position:'absolute', top: '50%', left:'50%', transform:'translate(-50%, -50%)',fontSize:'32px', fontWeight:'bold'}}>
+                                {/* {syncDelay.length ? 
+                                <div>{syncDelay[syncDelay.length-1]}s </div> 
+                                : '?'} */}
+                                </div>
+                            </div>
+                          </div>
+                          </div>
                     </div>
                          
                     </div>
